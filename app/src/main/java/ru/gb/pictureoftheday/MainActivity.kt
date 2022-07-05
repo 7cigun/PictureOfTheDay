82c5240d42fd6d10150bf13de614eeaec6cfd7a9
7cigun <7cigun@gmail.com>
2022-07-05 20:40:44 +0300

    Урок 2. Создал три темы, используя собственные кастомные атрибуты.

--- app/src/main/java/ru/gb/pictureoftheday/MainActivity.kt
+++ app/src/main/java/ru/gb/pictureoftheday/MainActivity.kt
@@ -1,15 +1,9 @@
 package ru.gb.pictureoftheday
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import ru.gb.pictureoftheday.databinding.ActivityMainBinding
 import ru.gb.pictureoftheday.view.PictureOfTheDayFragment
 
@@ -21,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.FlowerTheme)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
