9899816b2ec9f57b5d0655b1821eed8005b89955
7cigun <7cigun@gmail.com>
2022-08-05 19:39:11 +0300

    Урок 7. Побаловался с текствой радугой.

--- app/src/main/java/ru/gb/pictureoftheday/view/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/gb/pictureoftheday/view/picture/PictureOfTheDayFragment.kt
@@ -4,9 +4,15 @@ import android.content.Intent
 import android.graphics.Typeface
 import android.net.Uri
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import ru.gb.pictureoftheday.MainActivity
@@ -26,6 +32,8 @@ class PictureOfTheDayFragment : Fragment() {
     val todayDate = date.toString()
     val yesterdayDate = date.minus(Period.of(0, 0, 1)).toString()
     val TDBYDate = date.minus(Period.of(0, 0, 2)).toString()
+
+    lateinit var spannableRainbow: SpannableString
 
     private var _binding: FragmentPictureBinding? = null
     private val binding get() = _binding!!
@@ -107,7 +115,53 @@ class PictureOfTheDayFragment : Fragment() {
                 binding.ImageView.load(appState.pictureOfTheDayResponseData.url)
                 binding.textView.text = appState.pictureOfTheDayResponseData.explanation
                 binding.textView.typeface = Typeface.createFromAsset(requireActivity().assets,"fonts/acme.ttf")
+                spannableRainbow = SpannableString(appState.pictureOfTheDayResponseData.explanation)
+                rainbow(1)
             }
+        }
+    }
+
+    fun rainbow(i:Int=1) {
+        var currentCount = i
+        val x = object : CountDownTimer(20000, 200) {
+            override fun onTick(millisUntilFinished: Long) {
+                colorText(currentCount)
+                currentCount = if (++currentCount>3) 1 else currentCount
+            }
+            override fun onFinish() {
+                rainbow(currentCount)
+            }
+        }
+        x.start()
+    }
+
+
+    private fun colorText(colorFirstNumber:Int){
+        binding.textView.setText(spannableRainbow, TextView.BufferType.SPANNABLE)
+        spannableRainbow = binding.textView.text as SpannableString
+        val map = mapOf(
+            0 to ContextCompat.getColor(requireContext(), R.color.red),
+            1 to ContextCompat.getColor(requireContext(), R.color.orange),
+            2 to ContextCompat.getColor(requireContext(), R.color.yellow),
+            3 to ContextCompat.getColor(requireContext(), R.color.green),
+            4 to ContextCompat.getColor(requireContext(), R.color.blue),
+        )
+        val spans = spannableRainbow.getSpans(
+            0, spannableRainbow.length,
+            ForegroundColorSpan::class.java
+        )
+        for (span in spans) {
+            spannableRainbow.removeSpan(span)
+        }
+
+        var colorNumber = colorFirstNumber
+        for (i in 0 until binding.textView.text.length) {
+            if (colorNumber == 3) colorNumber = 0 else colorNumber += 1
+            spannableRainbow.setSpan(
+                ForegroundColorSpan(map.getValue(colorNumber)),
+                i, i + 1,
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+            )
         }
     }
 
